0a494f18658f3d7cf8664ea5c427dd46b5b9c50f
DarthSethious <sjaugmented@gmail.com>
2020-11-09 14:51:34 -0800

    can hold down mouse button for movement; moved followCam to LateUpdate

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,14 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
