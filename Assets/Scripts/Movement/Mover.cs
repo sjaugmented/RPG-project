6342363f7e8ba9824c85b49e10445b576d8667d7
DarthSethious <sjaugmented@gmail.com>
2020-11-10 12:05:12 -0800

    +PlayerController refator

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,38 +8,19 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    NavMeshAgent agent;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
-
         UpdateAnimator();
     }
 
-
-    private void MoveToCursor()
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        bool hasHit = Physics.Raycast(ray, out hit);
-
-        if (hasHit) agent.destination = hit.point;
+        GetComponent<NavMeshAgent>().destination = destination;
     }
+
     private void UpdateAnimator()
     {
-        Vector3 velocity = agent.velocity;
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
         Vector3 localVelocity = transform.InverseTransformDirection(velocity);
         float speed = localVelocity.z;
         GetComponent<Animator>().SetFloat("forwardSpeed", speed);
