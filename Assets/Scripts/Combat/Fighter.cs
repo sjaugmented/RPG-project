afcbfcff56c5168d1552b82dede1dd0de5ce55bc
DarthSethious <sjaugmented@gmail.com>
2020-11-10 13:25:22 -0800

    RPG.Combat +Fighter and CombatTarget

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack()
+        {
+            print("Take that you fucker!");
+        }
+    }
+}
