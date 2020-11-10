30896687e1999e533035f4145c44d85c66b3c913
DarthSethious <sjaugmented@gmail.com>
2020-11-10 13:43:14 -0800

    creating raycast hits for combat detection

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                Debug.Log(hit.transform);
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -17,15 +34,18 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
