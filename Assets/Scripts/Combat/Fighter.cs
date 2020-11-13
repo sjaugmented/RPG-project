e7f6288cc1379c4abb8c607db9895b755b3112bd
sjaugmented <sjaugmented@gmail.com>
2020-11-12 18:00:06 -0800

    +Health and dealing damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,9 +9,12 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] int attackDamage = 5;
         
         Transform target;
         Mover mover;
+        float timeSinceLastAttack = 0;
 
         private void Start()
         {
@@ -20,6 +23,8 @@ namespace RPG.Combat
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -29,13 +34,28 @@ namespace RPG.Combat
             else
             {
                 mover.Cancel();
-                GetComponent<Animator>().SetTrigger("attack");
+                AttackBehaviour();
             }
         }
 
         private bool GetIsInRange()
         {
             return Vector3.Distance(target.position, transform.position) < weaponRange;
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks) 
+            {
+                // This will trigger the Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        // animation event
+        void Hit() {
+            target.GetComponent<Health>().TakeDamage(attackDamage);
         }
 
         public void Attack(CombatTarget combatTarget)
@@ -49,9 +69,6 @@ namespace RPG.Combat
             target = null;
         }
 
-        // animation event
-        void Hit() {
-
-        }
+        
     }
 }
