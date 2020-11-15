5e2ce4012fbb2b2f110e55468dd4ab40aef59bd0
sjaugmented <sjaugmented@gmail.com>
2020-11-15 11:13:14 -0800

    +AI controller and distance check to player

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control 
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print(gameObject.name + " is chasing player");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");            
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+
+}
