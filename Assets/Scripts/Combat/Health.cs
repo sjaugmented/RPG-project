bde0317390cad1dc85fb72ba0a86ce3838acc200
sjaugmented <sjaugmented@gmail.com>
2020-11-15 09:02:24 -0800

    +death state and trigger

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -7,6 +7,15 @@ namespace RPG.Combat {
     public class Health : MonoBehaviour
     {
         [SerializeField] float health = 100f;
+        bool isAlive = true;
+
+        private void Update() 
+        {
+            if (health < 1 && isAlive) {
+                GetComponent<Animator>().SetTrigger("die");
+                isAlive = false;
+            }
+        }
 
         public void TakeDamage(float damage) 
         {
